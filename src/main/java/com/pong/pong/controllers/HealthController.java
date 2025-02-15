39ece8bf607dea8183f0fe8c6f1b5114997ea3b5
Jojan Esteban Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2025-02-15 01:35:49 -0500

    added

---
+++ src/main/java/com/pong/pong/controllers/HealthController.java
@@ -0,0 +1,13 @@
+package com.pong.pong.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("OK");
+    }
+}
